e14bf8cf358398a76bf5f592b570aaeccb6329b9
mDep07 <mdepdma@gmail.com>
2021-12-09 16:46:05 -0300

    Navbar

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,6 +1,67 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FiShoppingCart, FiStar, FiUser } from 'react-icons/fi';
 
-const Navbar = styled.nav`` 
+const Navbar = styled.nav`
+  --color: 3, 140, 253;
+  background-color: rgba(var(--color), .2);
+  padding: 5px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: space-between;
+`;
 
-export default () => <nav></nav>
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  & li:not(:first) {
+    margin-left: 1rem
+  }
+`;
+
+const ListItem = styled.li`
+  list-style-type: none;
+  display: inline-block;
+`;
+
+const ListItemButton = styled.a`
+  font-size: 1.5rem;
+  background-color: transparent;
+  color: rgb(var(--color));
+  display: flex;
+  padding: 8px;
+  border-radius: 8px;
+  cursor: pointer;
+  margin-left: ${(props) => (props.first ? 0 : '5px')};
+  &:hover {
+    background-color: rgb(var(--color), .3);
+    color: white;
+  }
+  &:active {
+    transform: scale(.95)
+  }
+`;
+
+export default () => (
+  <Navbar>
+    <List>
+      <ListItem first>
+        <ListItemButton>
+          <FiShoppingCart />
+        </ListItemButton>
+      </ListItem>
+      <ListItem>
+        <ListItemButton>
+          <FiStar />
+        </ListItemButton>
+      </ListItem>
+    </List>
+    <List>
+      <ListItem first>
+        <ListItemButton>
+          <FiUser />
+        </ListItemButton>
+      </ListItem>
+    </List>
+  </Navbar>
+);
